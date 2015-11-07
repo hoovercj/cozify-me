6473733a4f7ffdb9b293119b9d4aa25c454475de
Cody Hoover <login@codyhoover.com>
2015-11-07 18:57:35 +0000

    Added github authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151107132930) do
+ActiveRecord::Schema.define(version: 20151107175405) do
 
   create_table "rules", force: :cascade do |t|
     t.text     "rule_type"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20151107132930) do
     t.text     "code"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
