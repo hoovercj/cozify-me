6854a18e100a62b06afcfae76fb1745723fd2030
Cody Hoover <login@codyhoover.com>
2015-11-08 06:59:20 +0000

    Added production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+  config.github_key: <%= ENV["GITHUB_KEY"] %>
+  config.github_secret: <%= ENV["GITHUB_SECRET"] %>
+  
 end
