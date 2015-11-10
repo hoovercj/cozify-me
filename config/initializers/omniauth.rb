7ca6cce42b6f2707131831019c86980e6f60fef8
Cody Hoover <login@codyhoover.com>
2015-11-10 22:09:31 +0000

    Tweaked omniauth config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, Rails.application.secrets.github_key, Rails.application.secrets.github_secret
+  provider :github, Rails.application.secrets.github_key || ENV["GITHUB_KEY"], Rails.application.secrets.github_secret || ENV["GITHUB_SECRET"]
 end
